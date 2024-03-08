8e0fe3b50aa25136daf1f4f7ee858077e688db5d
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2024-03-08 23:06:47 +0330

    server inited

--- server/server.js
+++ server/server.js
@@ -1,1 +1,33 @@
-print("hello")
+const express = require('express')
+const cors = require("cors");
+const appRootPath = require("app-root-path")
+const mongoose = require('mongoose');
+const fileUpload = require("express-fileupload")
+
+app = express()
+
+require('dotenv').config({
+    path: appRootPath + "/.env"
+})
+
+app.use(cors())
+app.use("/static", express.static(appRootPath + "/public"))
+app.use(express.json({ extended: false }))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(fileUpload({
+    createParentPath: true,
+    limits: {
+        filesize: 2 * 1024 * 1024,
+    }
+}))
+
+mongoose.connect(process.env.DATABASE_URL , {authSource : "admin" }).then((res) => {
+   console.log("DB conected Ok!")
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => { console.log(`server is run at port ${PORT}`) })
