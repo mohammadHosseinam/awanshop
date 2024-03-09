7656f41dc77ce13a4bac787920fdce827bc08b5e
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2024-03-09 23:08:18 +0330

    admin handelers crated

---
+++ server/controller/admin/index.js
@@ -0,0 +1,29 @@
+const { Order } = require("../../model/Order");
+
+const showUnsentOrdersHandeler = async (req , res) => {
+    const unsenededOrders = await Order.find({status : "unsend"})
+    if (!unsenededOrders) return res.status(404).json({ massage: "سفارش جدیدی پیدا نشد" })
+    return res.status(200).json({unsenededOrders})
+}
+
+const showAllOrdersHandeler = async (req , res) => {
+    const unsenededOrders = await Order.find({})
+    if (!unsenededOrders) return res.status(404).json({ massage: "سفارشی ثبت نشده" })
+    return res.status(200).json({unsenededOrders})
+}
+
+const showReturnedOrdersHandeler = async(req , res) => {
+    const unsenededOrders = await Order.find({status : "returned"})
+    if (!unsenededOrders) return res.status(404).json({ massage: "سفارشی برگشت نخورده " })
+    return res.status(200).json({unsenededOrders})
+}
+
+const updateOrderStatusHandeler = async(req , res) => {
+    const order = await Order.findOne({_id : req.body.orderId})
+    const updatedORder = order
+    updatedORder.status = req.body.status
+    await Order.updateOne({_id : req.body.orderId} , updatedORder)
+    return res.status(200).json({message : "سفارش با موفقیت آپدیت شد"})
+}
+
+module.exports = { showAllOrdersHandeler , showUnsentOrdersHandeler , showReturnedOrdersHandeler , updateOrderStatusHandeler}
