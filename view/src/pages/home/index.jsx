a5d140b68c4c8084d08346b56b80be7f82dcd9ec
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2024-03-10 16:00:55 +0330

    (feature) hirosection for mobile

--- view/src/pages/home/index.jsx
+++ view/src/pages/home/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import awanLogo from "../../assets/images/awanLogo.png"
-import MoreIcon from '../../assets/icons/more/index.jsx'
-import BasketIcon from '../../assets/icons/basket/index.jsx'
-import UserIcon from '../../assets/icons/userIcon/index.jsx'
 import Header from '../../layout/header/index.jsx'
-//import MoreIcon from '../../assets/icons/more/index.jsx'
+import hiroBackground from "../../assets/images/hirimage.png"
 function Home() {
     return (
         <>
             <Header/>
+            <div style={{ background: `url(${hiroBackground})`, backgroundColor: "#000000", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", width: "100%" , height: "250px" }}>
+               <div className='flex justify-center items-center bg-black bg-opacity-55 h-full'>
+                   <h1 className='font-vazirmatn text-white'><span className='text-lime-800 text-xl font-medium'>آوان</span> به معنای طراوت و تازگیست</h1>
+               </div>
+            </div>
         </>
     )
 }
