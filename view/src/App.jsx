a44df75d9edeb2f092107ba920b9474b0a042249
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2024-03-11 10:56:21 +0330

    (core) router added to project

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/home'
+import { Route, Routes } from 'react-router-dom';
+import { routes } from './routes/index.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <Home/>
+    <Routes>
+      {Object.keys(routes).map((route) => {
+        return (
+          <Route
+            element={routes[route].element}
+            path={routes[route].path}
+          />
+        );
+      })}
+    </Routes>
   )
 }
 
---
+++ view/src/routes/index.jsx
@@ -0,0 +1,9 @@
+import  Home  from "../pages/home/index.jsx";
+
+export const routes = {
+    home:{
+        id:'1',
+        path:'/',
+        element:<Home/>
+    },
+}
