53d9ad70af65e8e0785b29905b807120693c6508
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2024-05-20 01:33:36 +0330

    (feature) admin page user info and create button

--- view/src/pages/admin/index.jsx
+++ view/src/pages/admin/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Header from '../../layout/header'
+import UserIcon from '../../assets/icons/userIcon'
+import PrimaryInlineButton from '../../components/PrimaryInlineButton'
 
 function AdminPanel() {
-  return (
-    <div>AminPanel</div>
-  )
+    return (
+        <>
+            <Header />
+            <div className='pt-7 px-6 pb-4'>
+                <div className='flex items-center gap-2 mb-3'>
+                    <UserIcon />
+                    <div>
+                        <h1 className='font-vazirmatn text-sm'>محمد حسین</h1>
+                        <p style={{ fontSize: "10px" }} className='font-vazirmatn font-light text-gray-500'>09032153600</p>
+                    </div>
+                </div>
+                <PrimaryInlineButton text="ساخت محصول جدید" onclick={()=>{console.log("clicked")}} className="text-xs px-2"/>
+            </div>
+
+        </>
+    )
 }
 
 export default AdminPanel
