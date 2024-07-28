4ed50d72218c3f879b865386baf1e39febecb8d8
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2024-07-29 01:24:02 +0330

    (feature): basket page responsived.

--- view/src/pages/basket/index.jsx
+++ view/src/pages/basket/index.jsx
@@ -2,22 +2,21 @@ import React from 'react'
 import Header from '../../layout/header'
 import PrimaryButton from '../../components/PrimaryButton'
 import BasketCard from './BasketCard'
-import OrderProductCart from '../../components/OrderProductCart'
+
 
 function Basket() {
     return (
         <div>
             <Header />
-            <div style={{ height: "calc(100vh - 144px)"}} className='pt-5 px-6 overflow-scroll scrollbar-hide'>
-                <h1 className='font-vazirmatn text-xs  text-slate-900 font-semibold mb-1'>سبد خرید شما</h1>
+            <div style={{ height: "calc(100vh - 144px)"}} className='pt-5 px-10 sm:px-12 md:px-24 lg:px-36 xl:px-48 overflow-scroll scrollbar-hide'>
+                <h1 className='font-vazirmatn text-xs sm:text-sm md:text-base md:font-medium  text-slate-900 font-semibold mb-1'>سبد خرید شما</h1>
                 <div style={{boxShadow : "0px 1px 4px rgba(0, 0, 0, 0.15)",borderRadius :"6px" , minHeight:"calc(100% - 35px)"}} className='flex flex-col gap-3 p-3'>
                     <BasketCard/>
                     <BasketCard/>
                     <BasketCard/>
-                    <OrderProductCart/>
                 </div>
             </div>
-            <div className='flex px-6 items-center h-16 shadow-inner z-10 bg-white'>
+            <div className='flex px-8 sm:px-10 md:px-12 lg:px-24 xl:px-32 items-center h-16 shadow-inner z-10 bg-white'>
                 <div className="grow">
                     <PrimaryButton text="تایید و تکمیل خرید" click={() => { console.log("add to basket") }} />
                 </div>
