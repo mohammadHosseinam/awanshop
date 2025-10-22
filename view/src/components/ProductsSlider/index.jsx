b275f3afa372dd9bee972f3ea44b4cfd45110b7c
mohammadHossein <mohammadhossdinahmadian@gmail.com>
2025-10-22 12:13:30 +0330

    (feature) : banners added.

--- view/src/components/ProductsSlider/index.jsx
+++ view/src/components/ProductsSlider/index.jsx
@@ -6,17 +6,23 @@ import "swiper/css/navigation";
 import { Navigation, Pagination } from "swiper/modules";
 import ProductCard from "../ProductCard";
 
-export default function ProductsSlider({Title}) {
+export default function ProductsSlider({ Title , color = "dark"}) {
+    const isDark = color === "dark";
+    const textColor = isDark ? "text-white" : "text-black";
+    const strokeColor = isDark ? "#FAFAFA" : "#111111";
+    const fillColor = isDark ? "#FAFAFA" : "#7C1616"
     return (
         <div className="mx-auto w-full">
-            <div className="flex items-center justify-between mb-8">
-                <h3 className="text-2xl font-bold text-white">{Title}</h3>
+            <div className="flex items-center justify-between mb-6">
+                <h3 className={`text-2xl font-bold ${textColor}`}>{Title}</h3>
                 <div className="flex gap-2">
-                    <button className="custom-prev [&.swiper-button-disabled]:opacity-50 [&.swiper-button-disabled]:brightness-75 transition hover:bg-white/10 p-2 rounded-full">
+                    <button className={`custom-prev p-2 rounded-full transition hover:bg-black/10 
+              [&.swiper-button-disabled]:opacity-50 
+              [&.swiper-button-disabled]:brightness-75`}>
                         <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="31" y="31" width="30" height="30" rx="7" transform="rotate(-180 31 31)" stroke="#FAFAFA" stroke-width="2" />
+                            <rect x="31" y="31" width="30" height="30" rx="7" transform="rotate(-180 31 31)" stroke={fillColor} stroke-width="2" />
                             <g clip-path="url(#clip0_341_245)">
-                                <path d="M12.59 20.59L14 22L20 16L14 10L12.59 11.41L17.17 16L12.59 20.59Z" fill="#FAFAFA" />
+                                <path d="M12.59 20.59L14 22L20 16L14 10L12.59 11.41L17.17 16L12.59 20.59Z" fill={fillColor} />
                             </g>
                             <defs>
                                 <clipPath id="clip0_341_245">
@@ -25,11 +31,13 @@ export default function ProductsSlider({Title}) {
                             </defs>
                         </svg>
                     </button>
-                    <button className="custom-next [&.swiper-button-disabled]:opacity-50 [&.swiper-button-disabled]:brightness-75 transition hover:bg-white/10 p-2 rounded-full">
+                    <button className={`custom-next p-2 rounded-full transition hover:bg-black/10 
+              [&.swiper-button-disabled]:opacity-50 
+              [&.swiper-button-disabled]:brightness-75`}>
                         <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="1" y="1" width="30" height="30" rx="7" stroke="#FAFAFA" stroke-width="2" />
+                            <rect x="1" y="1" width="30" height="30" rx="7" stroke={fillColor} stroke-width="2" />
                             <g clip-path="url(#clip0_341_243)">
-                                <path d="M19.41 11.41L18 10L12 16L18 22L19.41 20.59L14.83 16L19.41 11.41Z" fill="#FAFAFA" />
+                                <path d="M19.41 11.41L18 10L12 16L18 22L19.41 20.59L14.83 16L19.41 11.41Z" fill={fillColor} />
                             </g>
                             <defs>
                                 <clipPath id="clip0_341_243">
@@ -56,9 +64,9 @@ export default function ProductsSlider({Title}) {
                     1024: { slidesPerView: 5, spaceBetween: 24 }, // lg
                 }}
                 pagination={{ clickable: true }}
-                className="!pb-8" // برای فاصله دادن به pagination پایین
+                className="" 
             >
-                {[1, 2, 3, 4, 5 , 6 , 7 , 8 ,9 ,10 , 11].map((i) => (
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((i) => (
                     <SwiperSlide key={i}>
                         <div className=" ">
                             <ProductCard />
--- view/src/pages/home/index.jsx
+++ view/src/pages/home/index.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import Header from '../../layout/header/index.jsx'
 import HiroImage from "../../assets/images/hiroImage.png"
 import Shalvar from "../../assets/images/shalvar.png"
-import TrukIcon from '../../assets/icons/truckIcon/index.jsx'
-import DiscountIcon from '../../assets/icons/discountIcon/index.jsx'
-import ChangeIcon from '../../assets/icons/changeIcon/index.jsx'
-import BoxIcon from '../../assets/icons/boxIcon/index.jsx'
-import ProductCard from '../../components/ProductCard/index.jsx'
-import Sections from './sections/index.jsx'
+import Baner2 from "../../assets/images/baner2.png"
+import Baner1 from "../../assets/images/baner1.png"
 import Footer from '../../layout/footer/index.jsx'
 import SectionCard from '../../components/SectionCard/index.jsx'
 import ProductsSlider from '../../components/ProductsSlider/index.jsx'
@@ -50,61 +46,28 @@ function Home() {
                 <SectionCard Image={Shalvar} Name={"شلوار"} />
             </div>
             <div className='w-full px-20 mt-20'>
-                <div className='w-full py-5 flex flex-col px-5 items-center relative'>
+                <div className='w-full flex flex-col px-5 items-center relative'>
                     <svg className="absolute top-0 w-full h-auto -z-10" viewBox="0 0 1272 311" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-                        <path d="M0 32C0 20.7989 0 15.1984 2.17987 10.9202C4.09734 7.15695 7.15695 4.09734 10.9202 2.17987C15.1984 0 20.7989 0 32 0H1240C1251.2 0 1256.8 0 1261.08 2.17987C1264.84 4.09734 1267.9 7.15695 1269.82 10.9202C1272 15.1984 1272 20.7989 1272 32V213.946C1272 223.356 1272 228.061 1270.3 231.918C1268.8 235.309 1266.37 238.24 1263.32 240.337C1259.84 242.722 1255.18 243.598 1245.87 245.35C770.995 334.683 495.918 331.102 26.3009 245.236C16.9273 243.522 12.2404 242.665 8.74299 240.284C5.6672 238.189 3.22083 235.255 1.71372 231.853C0 227.984 0 223.254 0 213.794V32Z" fill="#7C1616"/>
+                        <path d="M0 32C0 20.7989 0 15.1984 2.17987 10.9202C4.09734 7.15695 7.15695 4.09734 10.9202 2.17987C15.1984 0 20.7989 0 32 0H1240C1251.2 0 1256.8 0 1261.08 2.17987C1264.84 4.09734 1267.9 7.15695 1269.82 10.9202C1272 15.1984 1272 20.7989 1272 32V213.946C1272 223.356 1272 228.061 1270.3 231.918C1268.8 235.309 1266.37 238.24 1263.32 240.337C1259.84 242.722 1255.18 243.598 1245.87 245.35C770.995 334.683 495.918 331.102 26.3009 245.236C16.9273 243.522 12.2404 242.665 8.74299 240.284C5.6672 238.189 3.22083 235.255 1.71372 231.853C0 227.984 0 223.254 0 213.794V32Z" fill="#7C1616" />
                     </svg>
-                    <ProductsSlider Title="محصولات شگفت انگیز"/>
+                    <ProductsSlider Title="محصولات شگفت انگیز" />
+                </div>
+            </div>
+            <div className='w-full flex px-20 justify-center gap-10 mt-10'>
+                <img src={Baner1} alt="" />
+                <img src={Baner2} alt="" />
+            </div>
+            <div className='w-full px-20 mt-10'>
+                <div className='w-full flex flex-col items-center relative'>
+                    <ProductsSlider Title="محصولات شگفت انگیز" color="light" />
+                </div>
+            </div>
+            <div className='w-full px-20 mt-10'>
+                <div className='w-full flex flex-col items-center relative'>
+                    <ProductsSlider Title="محصولات شگفت انگیز" color="light" />
                 </div>
             </div>
 
-            <Sections />
-            <h4 className="font-vazirmatn text-sm sm:text-base md:text-lg lg:text-2xl text-neutral-800 text-center mt-5 mb-4 font-medium">شلوار های زنانه</h4>
-            <div className='mx-6 sm:w-4/5 sm:mx-auto '>
-                <div className='flex flex-shrink-1 overflow-x-scroll scrollbar-hide sm:mx-auto'>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-6 sm:px-3">
-                        <ProductCard />
-                    </div>
-                </div>
-            </div>
-            <h4 className="font-vazirmatn text-sm sm:text-base md:text-lg lg:text-2xl text-neutral-800 text-center mt-5 mb-4 font-medium">شلوار های مردانه</h4>
-            <div className='mx-6 sm:w-4/5 sm:mx-auto mb-6'>
-                <div className='flex flex-shrink-1 overflow-x-scroll scrollbar-hide sm:mx-auto'>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-3 sm:px-3">
-                        <ProductCard />
-                    </div>
-                    <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 flex-shrink-0 px-6 sm:px-3">
-                        <ProductCard />
-                    </div>
-                </div>
-            </div>
             <Footer />
         </>
     )
